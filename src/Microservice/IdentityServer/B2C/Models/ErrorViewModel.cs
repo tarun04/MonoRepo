dfc21e18f0a684344e8296b4e39199ff6510ea1a
Tarun Reddy <tarunreddy.04@gmail.com>
2020-09-09 11:58:05 -0400

    Basic IdentityServer layout

---
+++ src/Microservice/IdentityServer/B2C/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonoRepo.Microservice.IdentityServer.B2C.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
